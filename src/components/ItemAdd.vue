03b78221bacc9ce02c33e7f856697cbb2f521a4a
Ryan Benson <benson.ryan@gmail.com>
2023-07-07 11:42:56 -0500

    Fixes tailwind include, adds start of new feedback

---
+++ src/components/ItemAdd.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({})
+</script>
+
+<template>
+  <div class="item item--new">
+    <input type="text" class="py-3 px-4 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400" placeholder="What feedback do you want to share?">
+  </div>
+</template>
+
+<style scoped>
+</style>
